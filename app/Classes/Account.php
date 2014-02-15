c256e4a301f886c7806b0b32c2ccbba52b73716d
Tuqiri <tuqiri@tuqiri.net>
2014-02-15 13:44:32 +0000

    Fixed the salt problem

--- app/Classes/Account.php
+++ app/Classes/Account.php
@@ -11,13 +11,26 @@ class Account {
         $user = Users::findFirst(array(
             array("username" => $username)
         ));
+        
         if($user == null)
         {
-            $this->error += array("login" => "Username is invalid");
+            $this->error = array("login" => "Username is invalid");
             return false;
         }
-        $salt = $user->getSalt();
-        $dbpassword = $user->getPassword();
+        
+        $password = Passwords::findFirst(array(
+            array("Userid" => $user->getid())
+        ));
+
+        //Cant find password this should not happen but is here just incase something bad happens
+        if($password == null)
+        {
+            $this->error = array("login" => "Password not found");
+            return false;
+        }
+        
+        $salt = $password->getSalt();
+        $dbpassword = $password->getPassword();
         
         //Hashing the password entered by the user then checking if it is the same in the database
         //If failed then return a login failed
@@ -25,11 +38,12 @@ class Account {
         
         if($dbpassword != $hashedpassword)
         {
-            $this->error += array("login" => "Password is incorrect");
+            $this->error = array("login" => "Password is incorrect");
+            ChromePhp::log($this->error);
             return false;
         }
         
-        
+        return true;
     }
     
     function Register($username, $passwordin, $email, $dob)
@@ -40,21 +54,25 @@ class Account {
         $user->setUsername($username);
         $user->setEmail($email);
         $user->setDateOfBirth($dob);
-        $user->setDateTime(time());
+        $user->setDateTime(microtime(false));
         
-        //Generating a random salt and converting it to UTF
-        $salt = mb_convert_encoding(mcrypt_create_iv(64, MCRYPT_DEV_URANDOM), "UTF-8", "ISO-8859-1");;
+        if(!($user->save()))
+            return false;
+        
+        //Generating a salt to be used with the password
+        $salt =  hash('sha256', $username . microtime() . $dob . "UgcrRZFBED91yqPecEXu");
         
         //Hashing the password with sha256 with a ranom salt the password and a random generated string value.
         //If the database is compromised they will need the random string in the code.
         $hashedpassword = hash('sha256', $salt . $passwordin);
         
+        $password->setUserid($user->getid());
         $password->setPassword($hashedpassword);
         $password->setSalt($salt);
-        $password->setPasswordChange(time());
-        $password->setLastLogin(time());
+        $password->setPasswordChange(microtime());
+        $password->setLastLogin(microtime());
         
-        if(!($user->save()) && !($password-save()))
+        if(!($password->save()))
             return false;
         
         return true;
