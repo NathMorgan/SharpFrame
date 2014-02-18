2be644daa1a6196294dcd2498505eeb0cf29fb78
Tuqiri <tuqiri@tuqiri.net>
2014-02-18 14:08:43 +0000

    Added a register and view actions

--- app/controllers/RoomController.php
+++ app/controllers/RoomController.php
@@ -2,14 +2,31 @@
 
 class RoomController extends ControllerBase 
 {
-    
-    
     public function indexAction()
     {
         
     }
     
+    public function registerAction()
+    {
+        $this->view->title = "New Room";
+        if ($this->request->isPost() == true)
+        {
+            $room = new Room();
+            $room->Register();
+        }
+    }
     
+    public function viewAction($roomid)
+    {
+        if(isset($roomid))
+        {
+            $room = new room();
+            $roomdetails = $room->getRoom($roomid);
+            $this->view->title = $roomdetails['name'];
+        }
+        $this->view->title = $roomid;
+    }
 }
 
 ?>
