996f0850a0bfafe2917acf7bec84b2f0a06964fa
Tuqiri <tuqiri@tuqiri.net>
2014-02-24 14:04:42 +0000

    Added authentication to come of the actions

--- app/controllers/RoomController.php
+++ app/controllers/RoomController.php
@@ -9,12 +9,20 @@ class RoomController extends ControllerBase
     
     public function registerAction()
     {
-        $this->view->title = "New Room";
+        //Checking if there is already a vailid session if not redirect back to homepage
+        if(Account::Authenticate($this->request->getClientAddress()) == null)
+        {
+            $this->response->redirect("");
+            $this->view->disable();
+        }
+        
         if ($this->request->isPost() == true)
         {
             $room = new Room();
             $room->Register();
         }
+        
+        $this->view->title = "New Room";
     }
     
     public function viewAction($roomid)
