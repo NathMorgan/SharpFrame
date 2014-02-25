e3c137bdd0ce3ccefa42a9a2e82e7eccfe7c3dc9
Tuqiri <tuqiri@tuqiri.net>
2014-02-25 15:24:00 +0000

    Added code to the controllerbase for DRY

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -4,5 +4,25 @@ use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller
 {
-
+    public $session;
+    public $authenticated;
+    public $user;
+    
+    public function initialize()
+    {
+        
+        $userid = Account::Authenticate($this->request->getClientAddress());
+        
+         if($userid != null)
+         {
+             $authenticated = true;
+             $user = Account::GetUser($userid);
+             $this->view->username = ucfirst($user->username);
+         }
+         else
+         {
+             $authenticated = false;
+             $this->view->username = "Guest";
+         }
+    }
 }
