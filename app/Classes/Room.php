8ce0712b996d0b34ea082ac635790ba116989bb1
Tuqiri <tuqiri@tuqiri.net>
2014-02-26 12:43:22 +0000

    Registering a new room now works

--- app/Classes/Room.php
+++ app/Classes/Room.php
@@ -3,23 +3,49 @@ class Room
 {
     public $error;
     
-    public function Register($name, $description, $videoid, $roomicon, $userid)
+    public function Register($title, $description, $videolink, $iconfile, $userid)
     {
-        $room = new Rooms();
-        $roomOwner = new Room_Owners();
+        //Checking to see if any errors is set or multiple files are uploaded
+        if (!isset($iconfile['error']) || is_array($iconfile['error']))
+        {
+            $this->error = "There is an error with the file that is uploaded";
+            return false;
+        }
         
-        $room->setRoomName($name);
+        //Pattern refferenced from http://www.mkyong.com/regular-expressions/how-to-validate-image-file-extension-with-regular-expression/
+        if(!preg_match("([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)", $iconfile['name']))
+        {
+            $this->error = "The file uploaded is not an image";
+            return false;
+        }
+        
+        //Getting the video id from the youtube link the regex code is refferenced from http://stackoverflow.com/questions/3392993/php-regex-to-get-youtube-video-id
+        $videoid = "";
+        if (!preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $videolink, $videoid))
+        {
+            $this->error = "The youtube link is invalid";
+            return false;
+        }
+        
+        //Moving the icon file from the temp location to the new location
+        move_uploaded_file($iconfile['tmp_name'], "/var/www/sharpframe.co.uk/public_html/public/content/icons/".$iconfile['name']);
+        
+        //Adding the details to the database
+        $room = new Rooms();
+        $roomowner = new RoomOwners();
+        
+        $room->setTitle($title);
         $room->setDiscription($description);
-        $room->setVideoid($videoid);
-        $room->setRoomIcon($roomicon);
+        $room->setVideoid(array_shift($videoid));
+        $room->setIcon($iconfile['name']);
         
         if(!($room->save()))
             return false;
         
-        $roomOwner->setRoomid((string)$room->getid());
-        $roomOwner->setUserid($userid);
+        $roomowner->setRoomid((string)$room->getid());
+        $roomowner->setUserid($userid);
         
-        if(!($roomOwner->save()))
+        if(!($roomowner->save()))
             return false;
         
         return true;
