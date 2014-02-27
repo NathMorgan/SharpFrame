7005ac6bc89e7a77d4050c2f09969ec91973ffb5
Tuqiri <tuqiri@tuqiri.net>
2014-02-27 10:19:31 +0000

    The index controller returns the room objects

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,7 +5,10 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
+        $room = new Room();
+        
         $this->view->title = "Home";
+        $this->view->rooms = $room->getRooms();
     }
 
 }
